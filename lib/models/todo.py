a83afd22aec620e6113338c72b45fbfa15100ed1
hildamu <mukamihilda437@gmail.com>
2024-06-12 17:42:38 +0300

    add task to table

---
+++ lib/models/todo.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS tasks
+                 (task text, completed integer)''')
+    conn.commit()
+    conn.close()
+
+def add_task(task):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks VALUES (?, 0)", (task,))
+    conn.commit()
+    conn.close()
+    print("Added successfully!")
